416152511447e0bd0871d927c960c039bfe41ede
tushpush25 <tusharmahli007@gmail.com>
2025-07-01 12:53:21 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+     String greeting = getGreeting();
+     System.out.println(greeting);
+
+    }
+
+    public static String getGreeting(){
+        return "Hello! Welcome to Java!";
+    }
+
+    public static int addNumber(int num1,int num2){
+        return num1+num2;
+    }
+}
